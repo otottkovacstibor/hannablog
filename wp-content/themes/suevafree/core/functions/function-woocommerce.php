57cb7ae48947f3e7f6d4242e0352b0c7029e985a
Jenkins <tibijenkins@zoho.com>
2018-11-23 19:14:31 +0000

    Sync 2018-11-23 20:14:31 [skip ci]

--- wp-content/themes/suevafree/core/functions/function-woocommerce.php
+++ wp-content/themes/suevafree/core/functions/function-woocommerce.php
@@ -227,4 +227,34 @@ if ( ! function_exists( 'suevafree_get_wc_product_thumbnail' ) ) {
 	
 }
 
+/*-----------------------------------------------------------------------------------*/
+/* Replace wc_get_gallery_image_html function */ 
+/*-----------------------------------------------------------------------------------*/ 
+
+if ( ! function_exists( 'suevafree_wc_get_gallery_image_html' ) ) {
+	
+	function suevafree_wc_get_gallery_image_html( $attachment_id, $main_image = false ) {
+		$flexslider        = (bool) apply_filters( 'woocommerce_single_product_flexslider_enabled', get_theme_support( 'wc-product-gallery-slider' ) );
+		$gallery_thumbnail = wc_get_image_size( 'gallery_thumbnail' );
+		$thumbnail_size    = apply_filters( 'woocommerce_gallery_thumbnail_size', array( $gallery_thumbnail['width'], $gallery_thumbnail['height'] ) );
+		$image_size        = apply_filters( 'woocommerce_gallery_image_size', $flexslider || $main_image ? 'woocommerce_single' : $thumbnail_size );
+		$full_size         = apply_filters( 'woocommerce_gallery_full_size', apply_filters( 'woocommerce_product_thumbnails_large_size', 'full' ) );
+		$thumbnail_src     = wp_get_attachment_image_src( $attachment_id, $thumbnail_size );
+		$full_src          = wp_get_attachment_image_src( $attachment_id, $full_size );
+		$image             = wp_get_attachment_image( $attachment_id, $image_size, false, array(
+			'title'                   => get_post_field( 'post_title', $attachment_id ),
+			'data-caption'            => get_post_field( 'post_excerpt', $attachment_id ),
+			'data-src'                => $full_src[0],
+			'data-large_image'        => $full_src[0],
+			'data-large_image_width'  => $full_src[1],
+			'data-large_image_height' => $full_src[2],
+			'class'                   => $main_image ? 'wp-post-image' : '',
+		) );
+	
+		return '<div data-thumb="' . esc_url( $thumbnail_src[0] ) . '" class="product-thumbnail woocommerce-product-gallery__image"><a data-rel="prettyPhoto[product-gallery]" href="' . esc_url( $full_src[0] ) . '">' . $image . '</a></div>';
+	}
+
+}
+
+
 ?>
