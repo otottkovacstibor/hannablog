ce99e2cb0f3a1f17900b7f09711dc5b6a6be9935
Tibor Otott-Kovacs <tibi.93@gmail.com>
2019-04-28 18:13:11 +0200

    Delete wp-config-extra.php

--- wp-config-extra.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-     $http_x_headers = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-     $_SERVER['REMOTE_ADDR'] = $http_x_headers[0];
-}
-
-?>
