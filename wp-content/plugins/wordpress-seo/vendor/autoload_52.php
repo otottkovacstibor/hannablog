2513ae4b44c794806fa118d79b26cc856564c54f
oktibor-ci <ci@oktibor.com>
2019-09-20 16:23:01 +0000

    Sync 2019-09-20 18:23:00 [skip ci]

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit435bab2df2dafaecf9d72bfc5a70d057::getLoader();
+return ComposerAutoloaderInitfdc8e1aa8bc8b17387177f809f2e6807::getLoader();
