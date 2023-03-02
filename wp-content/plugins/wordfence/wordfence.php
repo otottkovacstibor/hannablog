c8a4f19aa132b8f5e50ede171de249429ddd47f0
oktibor-ci <ci@oktibor.com>
2023-03-02 17:23:02 +0000

    Sync 2023-03-02 18:23:01 [skip ci]

--- wp-content/plugins/wordfence/wordfence.php
+++ wp-content/plugins/wordfence/wordfence.php
@@ -4,7 +4,7 @@ Plugin Name: Wordfence Security
 Plugin URI: http://www.wordfence.com/
 Description: Wordfence Security - Anti-virus, Firewall and Malware Scan
 Author: Wordfence
-Version: 7.9.0
+Version: 7.9.1
 Author URI: http://www.wordfence.com/
 Text Domain: wordfence
 Domain Path: /languages
@@ -38,8 +38,8 @@ if(defined('WP_INSTALLING') && WP_INSTALLING){
 if (!defined('ABSPATH')) {
 	exit;
 }
-define('WORDFENCE_VERSION', '7.9.0');
-define('WORDFENCE_BUILD_NUMBER', '1676401566');
+define('WORDFENCE_VERSION', '7.9.1');
+define('WORDFENCE_BUILD_NUMBER', '1677680064');
 define('WORDFENCE_BASENAME', function_exists('plugin_basename') ? plugin_basename(__FILE__) :
 	basename(dirname(__FILE__)) . '/' . basename(__FILE__));
 
