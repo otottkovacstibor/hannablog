6acec6a916718939d28da59347529cf7a250ae07
oktibor-ci <ci@oktibor.com>
2023-06-01 16:23:01 +0000

    Sync 2023-06-01 18:23:01 [skip ci]

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"%w){:(!Y4D(b=W0fFM,CrLZl deq^p6EAd.7j.z<Zgph;z;9bk*%7 D;!Z1L6*a*";s:7:"version";s:5:"1.0.5";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1148;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"%w){:(!Y4D(b=W0fFM,CrLZl deq^p6EAd.7j.z<Zgph;z;9bk*%7 D;!Z1L6*a*";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1148;}
